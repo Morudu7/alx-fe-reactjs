74ef793f8ba6ed503f89ce7d72943a3f242cc48e
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-07-12 00:30:02 +0200

    Modifying a pre-built react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,29 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
  return (
+  <>
    <div className="App">
     {/* The message components is rendered here*/}
     <WelcomeMessage />
   </div>
+
+  <div className="App">
+    <Header />
+  </div>
+
+  <div className="App">
+    <MainContent />
+  </div>
+
+  <div className="App">
+    <Footer />
+  </div>
+  </>
  );
 
 } 
