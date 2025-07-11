ebba85566b7481500be49c959c12dc81b2fa94bb
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-07-11 13:28:44 +0200

    Modifying a pre-build react app

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone,I am learning React at ALX!</h1>
+            <P>I am learning about JSX!</P>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
