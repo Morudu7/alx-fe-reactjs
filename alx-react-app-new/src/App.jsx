59588f2dd788de1f78c081838f89bd74ee2e653e
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-07-17 00:57:54 +0200

    Applying inline CSS styles to React components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,39 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+function App() {
+ return (
+  <>
+   <div className="App">
+    {/* The message components is rendered here*/}
+    <WelcomeMessage />
+  </div>
+
+  <div className="App">
+    <Header />
+  </div>
+
+  <div className="App">
+    <MainContent />
+  </div>
+
+  <div className="App">
+    <Footer />
+  </div>
+
+  <div className="App">
+    <UserProfile
+    name="Alice"
+    age="25"
+    bio="Loves hiking and photography" />
+  </div>
+  </>
+ );
+
+} 
+
+export default App;
---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return(
+        <footer style= {{backgroundColor: 'navy', textAlign: 'centre'}}>
+        <p style= {{color: 'white'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
