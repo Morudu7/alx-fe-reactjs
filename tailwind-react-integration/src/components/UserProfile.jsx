7e02feaf5ef62cc708d7185a9be4ff1e6912004c
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-08-04 19:50:28 +0200

    basic tailwindcss styles

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm-400 mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" class="rounded-full w-150 h-150 mx-auto" />
+      <img src="https://via.placeholder.com/150" alt="User" class="rounded-full w-36 h-36 mx-auto" />
       <h1 class="tex-xl text-blue-800 my-4">John Doe</h1>
       <p class="tex-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
