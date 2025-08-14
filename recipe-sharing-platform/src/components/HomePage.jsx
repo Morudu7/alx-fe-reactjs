5e918c33d2a6683dbe32d39c46e793e9f3eb7a33
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-08-14 23:28:31 +0200

    Building the Recipe Detail Page & using useEffect

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import recipesData from '../data.json'
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
     const [recipes, setRecipes] = useState([]);
@@ -31,12 +32,12 @@ useEffect (() => {
               <div className="p-6">
                 <h2 className="text-2xl font-semibold text-gray-900 mb-3">{recipe.title}</h2>
                 <p className="text-gray-700 mb-4">{recipe.summary}</p>
-                <a 
-                  href={`/recipe/${recipe.id}`} 
+              
+                  <Link to={`/recipe/${recipe.id}`}
                   className="inline-block bg-cyan-500 text-white font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-cyan-600 transition-colors duration-300"
-                >
+                  >
                   View Recipe
-                </a>
+                  </Link>
               </div>
             </div>
           ))}
